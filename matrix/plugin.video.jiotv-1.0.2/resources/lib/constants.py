5e2ca7551b2eb3d0bdbedf5d19a5d3de5a47ba5c
JediMasterYodaa <nikhil13@somaiya.edu>
2023-02-23 13:18:12 +0530

    Updated zips

--- matrix/plugin.video.jiotv-1.0.2/resources/lib/constants.py
+++ matrix/plugin.video.jiotv-1.0.2/resources/lib/constants.py
@@ -11,7 +11,7 @@ IMG_CATCHUP = "http://jiotv.catchup.cdn.jio.com/dare_images/images/"
 IMG_CATCHUP_SHOWS = "http://jiotv.catchup.cdn.jio.com/dare_images/shows/"
 PLAY_URL = "plugin://plugin.video.jiotv/resources/lib/main/play/?"
 FEATURED_SRC = "https://tv.media.jio.com/apis/v1.6/getdata/featurednew?start=0&limit=30&langId=6"
-CHANNELS_SRC = "https://sagraecdnems05.cdnsrv.jio.com/jiotv.data.cdn.jio.com/apis/v3.0/getMobileChannelList/get/?langId=6&os=android&devicetype=phone&usertype=JIO&version=290&langId=6"
+CHANNELS_SRC = "https://jiotv.data.cdn.jio.com/apis/v3.0/getMobileChannelList/get/?langId=6&os=android&devicetype=phone&usertype=JIO&version=290&langId=6"
 GET_CHANNEL_URL = "https://jiotvapi.media.jio.com/playback/apis/v1/geturl?langId=6"
 CATCHUP_SRC = "https://jiotv.data.cdn.jio.com/apis/v1.3/getepg/get?offset={0}&channel_id={1}&langId=6"
 M3U_SRC = os.path.join(translatePath(
